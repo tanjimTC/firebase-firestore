a4c93ad2882d8d0b9b07672d6d21dfe4f1cbb827
tanjimTC <tanjimtc74@gmail.com>
2020-09-19 02:56:15 +0600

    firebase store with rea time update

--- app.js
+++ app.js
@@ -6,25 +6,36 @@ function rederCafe(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let cross = document.createElement("div");
 
   li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = "x";
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  //   deleting data
+  cross.addEventListener("click", (e) => {
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("cafes").doc(id).delete();
+  });
 }
 
 // getting data
-db.collection("cafes")
-  .get()
-  .then((querySnapshot) => {
-    querySnapshot.docs.forEach((doc) => {
-      rederCafe(doc);
-    });
-  });
+// db.collection("cafes")
+//   //   .where("city", "==", "Sylhet")
+//   //   .orderBy("name")
+//   .get()
+//   .then((querySnapshot) => {
+//     querySnapshot.docs.forEach((doc) => {
+//       rederCafe(doc);
+//     });
+//   });
 
 // saving data
 form.addEventListener("submit", (e) => {
@@ -36,3 +47,33 @@ form.addEventListener("submit", (e) => {
   form.name.value = "";
   form.city.value = "";
 });
+
+// getting realtime data
+
+db.collection("cafes")
+  .orderBy("name")
+  .onSnapshot((snapshot) => {
+    let changes = snapshot.docChanges();
+    changes.forEach((element) => {
+      if (element.type == "added") {
+        rederCafe(element.doc);
+      } else if (element.type == "removed") {
+        let li = cafeList.querySelector("[data-id=" + element.doc.id + "]");
+        cafeList.removeChild(li);
+      }
+    });
+  });
+
+// updating records (console demo)
+// db.collection('cafes').doc('DOgwUvtEQbjZohQNIeMr').update({
+//     name: 'mario world'
+// });
+
+// db.collection('cafes').doc('DOgwUvtEQbjZohQNIeMr').update({
+//     city: 'hong kong'
+// });
+
+// setting data
+// db.collection('cafes').doc('DOgwUvtEQbjZohQNIeMr').set({
+//     city: 'hong kong'
+// });
